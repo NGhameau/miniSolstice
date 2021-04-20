a26d9803e2fb2d6266abd219f5a47ae9fa6f3e41
hameau <a.hameau@hotmail.fr>
2021-04-20 15:14:07 +0200

    add date to temporary index.html

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
+
+var listItems = ["1","2","3"]
+
+
+app.get("/", function(req,res){
+  //res.send("<h1>HELLO</h1>");
+  //res.sendFile(__dirname+"/index.html")
+  res.render('index',{
+    listItems: listItems
+  })
+
+})
+
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+  console.log(item);
+  listItems.push(item);
+
+  res.redirect("/")
+})
+
+
+app.listen(3000,function(){
+  console.log("Server running on port 3000");
+})
