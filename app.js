da34be4f037d5b2ec069ea5a3dd58d65a8d4981c
hameau <a.hameau@hotmail.fr>
2021-04-24 14:31:14 +0200

    add DB

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require("mongoose");
+
+
+
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
-const ejs = require('ejs')
+
 app.set('view engine', 'ejs')
 
 
+
+//mongoose.connect("mongodb+srv://admin-angie:test123@cluster0.gjrjw.mongodb.net/todolistDB", {
+mongoose.connect("mongodb://admin:EFCvpa26526@node84005-todolist.hidora.com:27017/todolistDB", {
+//mongoose.connect("mongodb://mongo:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+
+
+
+
 var listItems = ["1","2","3"]
+
+
+
 
 
 app.get("/", function(req,res){
